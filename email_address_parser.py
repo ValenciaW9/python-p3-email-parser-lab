cca2d55aa75e071b28b4b654c2718445618b1852
Valencia Walker <129445679+ValenciaW9@users.noreply.github.com>
2024-02-27 17:31:08 -0500

    python-p3-email-parser-lab/
    python-p3-email-parser-lab/

--- email_address_parser.py
+++ email_address_parser.py
@@ -2,29 +2,17 @@
 
 import re
 
+def parse(self):
+    addresses = re.split(r",|\s", self.email_addresses)
 
-class EmailAddressParser:
-    def __init__(self, email_addresses):
-        self.email_addresses = email_addresses
+    parsed_addresses = []
+    for address in addresses:
+        address = address.strip()
+        if address and not any(address in parsed for parsed in parsed_addresses):
+            parsed_addresses.append(address)
 
-    def parse(self):
-        # Split the input string using a regular expression to handle both comma separation and space separation
-        addresses = re.split(r",|\s", self.email_addresses)
-
-        parsed_addresses = []
-        for address in addresses:
-            # Remove any leading or trailing whitespaces from each email address
-            address = address.strip()
-            if address and address not in parsed_addresses:
-                parsed_addresses.append(address)
-
-        # Sort the email addresses alphabetically
-        parsed_addresses.sort()
-
-        return parsed_addresses
-
-
-
+    parsed_addresses.sort()
+    return parsed_addresses
 
 
 #email_addresses = "john@doe.com, person@somewhere.org"
